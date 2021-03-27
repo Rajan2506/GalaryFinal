398f7a0021012a4c00c30ed3512ac5ffcb9bb2a1
Rajan <rajan2506bhagat@gmail.com>
2021-03-27 16:58:07 -0700

    Initial Commit

---
+++ GalaryFinal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GalaryFinal
+//
+//  Created by user190507 on 3/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
